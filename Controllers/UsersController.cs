3a93d8f513a51e310e48a38b03826c1dac189bd8
usman6666ahmed <usman.imtiaz@devsinc.com>
2022-09-16 00:56:04 +0500

    Custom Controller
    * Created a new "Users" controller
    * Removed rule from csproj
    * Updated gitignore

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+
+  [ApiController]
+  [Route("[controller]")]
+  public class UsersController : ControllerBase
+  {
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+      _context = context;
+    }
+
+    //  /users/
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+    {
+      return await _context.Users.ToListAsync();
+    }
+
+    //  /users/:id
+    [HttpGet("{id}")]
+    public async Task<ActionResult<User>> GetUser(int id)
+    {
+      return await _context.Users.FindAsync(id);
+    }
+  }
+}
